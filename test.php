5a77b18a13d1abfab7b8f7d22a5f5e321051c85f
robertalford <r.m.l.alford@gmail.com>
2016-04-10 15:55:19 +1000

    test db on prod

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
  
-echo 'http://' . $_SERVER['SERVER_NAME'];
+include './config.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// GET the data
+$sql = "SELECT * FROM messages;
+$result = $conn->query($sql);
+
+echo $result;
+
 ?>
