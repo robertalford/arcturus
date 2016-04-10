e48c2a115c9c014aff8d6a7636b0284ae8ec3c07
Rob Alford <rob.alford@xive.com.au>
2016-04-10 13:16:09 +1000

    my second committ

---
+++ arcturus/test.php
@@ -0,0 +1,17 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+// GET the data
+$sql = "SELECT * FROM messages";
+$result = $conn->query($sql);
+
+echo $result;
+
+?>
